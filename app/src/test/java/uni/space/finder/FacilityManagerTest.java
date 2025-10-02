c3bd34ecf551115088bdaa48759e9c914ee7089d
clive897 <nathan.nourse@student.uts.edu.au>
2025-10-02 16:06:23 +1000

    backend updated

---
+++ app/src/test/java/uni/space/finder/FacilityManagerTest.java
@@ -0,0 +1,14 @@
+package uni.space.finder;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FacilityManagerTest {
+    @Test
+    void testAddFacility() {
+        FacilityManager manager = new FacilityManager();
+        Facility facility = new Facility("Projector", true);
+        manager.addFacility(facility);
+        assertTrue(manager.getFacilities().contains(facility));
+    }
+}
---
+++ app/src/test/java/uni/space/finder/RoomTest.java
@@ -0,0 +1,13 @@
+package uni.space.finder;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoomTest {
+    @Test
+    void testRoomCreation() {
+        Room room = new Room("Room101", 20);
+        assertEquals("Room101", room.getName());
+        assertEquals(20, room.getCapacity());
+    }
+}
